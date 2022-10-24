f0bef3630c6d73f66e546ca06e1f84b8a2832a82
aefe2 <shigaev_1990@mail.ru>
2022-10-24 12:11:30 +0700

    Defined model "Genre"

--- locallibrary/locallibrary/catalog/models.py
+++ locallibrary/locallibrary/catalog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Genre(models.Model):
+    """
+    Model representing a book genre (e.g. Science Fiction, Non Fiction).
+    """
+    name = models.CharField(max_length=200, help_text="Enter a book genre (e.g. Science Fiction, French Poetry etc.)")
+
+    def __str__(self):
+        """
+        String for representing the Model object (in Admin site etc.)
+        """
+        return self.name
