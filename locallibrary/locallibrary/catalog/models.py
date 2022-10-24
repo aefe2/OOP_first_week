8fe2d44ca3a56b871f72d50d6ec1955f82bcdcd1
Kakayou <shigaev_1990@mail.ru>
2022-10-24 22:48:44 +0700

    Updated classes(AuthorAdmin, BookInst-e, BookAdmin, BookInstanceAdmin) in admin.py

--- locallibrary/locallibrary/catalog/models.py
+++ locallibrary/locallibrary/catalog/models.py
@@ -60,6 +60,16 @@ class Book(models.Model):
         """String for representing the Model object."""
         return self.title
 
+    def display_genre(self):
+        """
+        Creates a string for the Genre. This is required to display genre in Admin.
+        В нем создаётся строка из первых трёх значений поля genre (если они существуют) и short_description,
+        которое может быть использовано в админ-панели.
+        """
+        return ', '.join([genre.name for genre in self.genre.all()[:3]])
+
+    display_genre.short_description = 'Genre'
+
 
 class BookInstance(models.Model):
     """Model representing a specific copy of a book (i.e. that can be borrowed from the library)."""
