15d6d43224c030d28c86705c3c15653cee50ac69
aefe2 <shigaev_1990@mail.ru>
2022-10-24 12:19:04 +0700

    Defined model "Author"

--- locallibrary/locallibrary/catalog/models.py
+++ locallibrary/locallibrary/catalog/models.py
@@ -74,3 +74,25 @@ class BookInstance(models.Model):
         String for representing the Model object
         """
         return '%s (%s)' % (self.id, self.book.title)
+
+
+class Author(models.Model):
+    """
+    Model representing an author.
+    """
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField('Died', null=True, blank=True)
+
+    def get_absolute_url(self):
+        """
+        Returns the url to access a particular author instance.
+        """
+        return reverse('author-detail', args=[str(self.id)])
+
+    def __str__(self):
+        """
+        String for representing the Model object.
+        """
+        return '%s, %s' % (self.last_name, self.first_name)
