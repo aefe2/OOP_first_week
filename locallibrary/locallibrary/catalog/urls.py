369658fb7ad93434d3a806587ba3bbaf644bb19c
aefe2 <shigaev_1990@mail.ru>
2022-10-25 11:24:29 +0700

    Added pattern to catalog/urls

--- locallibrary/locallibrary/catalog/urls.py
+++ locallibrary/locallibrary/catalog/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     url(r'^$', views.index, name='index'),
     url(r'^books/$', views.BookListView.as_view(), name='books'),
+    url(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
 ]
