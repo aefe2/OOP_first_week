ca1413872d2cec3ccab07b74ba5b68f755e51288
aefe2 <shigaev_1990@mail.ru>
2022-10-26 09:40:03 +0700

    Created login.html, views.py - created num of visits

--- locallibrary/locallibrary/catalog/views.py
+++ locallibrary/locallibrary/catalog/views.py
@@ -10,19 +10,22 @@ def index(request):
     # Генерация "количеств" некоторых главных объектов
     num_books = Book.objects.all().count()
     num_instances = BookInstance.objects.all().count()
+    num_genres = Genre.objects.count()
     # Доступные книги (статус = 'a')
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
     num_authors = Author.objects.count()  # Метод 'all()' применён по умолчанию.
-    num_genres = Genre.objects.count()
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits+1
 
     # Отрисовка HTML-шаблона index.html с данными внутри
     # переменной контекста context
     return render(
         request,
         'index.html',
-        context={'num_books': num_books, 'num_instances': num_instances,
+        context={'num_books': num_books, 'num_instances': num_instances, 'num_genres': num_genres,
                  'num_instances_available': num_instances_available, 'num_authors': num_authors,
-                 'num_genres': num_genres},
+                 'num_visits': num_visits},
     )
 
 
--- locallibrary/locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/locallibrary/urls.py
@@ -33,3 +33,7 @@ urlpatterns += [
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += [
+    path('accounts/', include('django.contrib.auth.urls')),
+]
